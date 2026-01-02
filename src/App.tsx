2118298a03f6f43952427595395276b3b76cc172
Muhammad Fuaad <muhmmad.fuaad@strateger.ai>
2026-01-02 22:00:21 +0500

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Routes, Route } from "react-router-dom";
 import WriteMessage from "./components/WriteMessage.tsx";
 
 function App() {
-
-    return (
-        <><WriteMessage /></>
-    );
+  return (
+    <Routes>
+      <Route path="/write-message" element={<WriteMessage />} />
+    </Routes>
+  );
 }
 
 export default App;
